6bac7228d562ce21bc79fb1e4eeeaf1205035628
hyewon.yeon <hyewon.yeon@mobyus.com>
2026-04-01 10:35:54 +0900

    초기 세팅

---
+++ src/test/java/com/tripflow/tripflow/TripFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripflow.tripflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
